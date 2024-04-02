167a6e1a3eedcccc64e4c1296c407121958c53fa
Brianca Knight <briancaknight@gmail.com>
2024-04-02 10:34:57 -0700

    apply colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 
 export default function Home() {
   return (
-    <h1 className="">
+   <div>
+    <h1 className="border-2 border-vuejs-932">
     Hello world!
   </h1>
+  <h1 className="text-vuejs-100">
+    Hello world!
+  </h1>
+  </div>
   );
 }
